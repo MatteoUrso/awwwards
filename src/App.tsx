e72dd8b4cd598fd194d460ea2c103d4d4589ac33
INFOR\matteou <m.urso@esaedro.com>
2025-03-14 11:06:20 +0100

    add Features component with interactive BentoTilt and BentoCard; update App component to include Features section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { About } from "./components/about";
+import { Features } from "./components/features";
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
